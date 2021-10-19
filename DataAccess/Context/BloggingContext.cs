b54c6ac21716825062a02f83684c71c4b48fda32
Ryan Attard <ryanattard@gmail.com>
2021-10-19 13:56:16 +0200

    updated libraries, finalized bloggingcontext

--- DataAccess/Context/BloggingContext.cs
+++ DataAccess/Context/BloggingContext.cs
@@ -6,10 +6,22 @@ using System.Text;
  
 namespace DataAccess.Context
 {
+    //this is the class which is the gateway to our database
+    //this inherits from a built-in class called xxxDbContext
+    //the dbcontext class contains methods which will allow us to
+    //amongst other things to connect with the database, add an item to the database
+    //delete, update, querying using LINQ....
     public class BloggingContext : DbContext
     {
-        public DbSet<Blog> Blogs { get; set; }
+        public BloggingContext(DbContextOptions<BloggingContext> options):
+            base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }  //DbSet will eventually be converted into a table
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
       
     }
 }
