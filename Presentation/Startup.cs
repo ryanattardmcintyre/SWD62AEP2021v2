905477201864b56821038b682f6f4b84dcd0cf78
Ryan Attard <ryanattard@gmail.com>
2021-11-15 09:56:38 +0100

    Login complete

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Presentation.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -40,11 +39,7 @@ namespace Presentation
               options.UseSqlServer(
                   Configuration.GetConnectionString("DefaultConnection")));
 
-           //services.AddDbContext<ApplicationDbContext>(options =>
-           //     options.UseSqlServer(
-           //        Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<BloggingContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
